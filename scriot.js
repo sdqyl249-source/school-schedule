f08fae482bd23761a45e2988a035558fd3428289
aqeel saad <sdqyl249@gmail.com>
2026-07-17 00:16:58 +0300

    Refactor scriot.js for improved functionality

--- scriot.js
+++ scriot.js
@@ -6,10 +6,7 @@ function showPage(id) {
     }
     document.getElementById(id).style.display = 'block';
 }
- <script> 
      window.database = firebase.database();
-<script>
-<script src="scriot.js"><script>
 // 2. تحديث التاريخ والوقت
 function updateDateTime() {
     const now = new Date();
